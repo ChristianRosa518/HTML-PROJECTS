e75681ba41350e57cc805914d425daec13af5427
ChristianRosa518 <chrisrosa815518@gmail.com>
2024-07-13 19:58:06 -0400

    First Commit

---
+++ WeedRater/client/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(5);
+
+  const objer: { length: number } = { length: 4 };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        {objer.length} {objer.length > 1 ? "videos" : "video"}{" "}
+      </p>
+    </>
+  );
+}
+
+export default App;
