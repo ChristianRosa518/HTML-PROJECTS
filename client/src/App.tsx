faba4c9f5f1cd5cc617031d02dae9ba5fd6fc36d
ChristianRosa518 <chrisrosa815518@gmail.com>
2024-07-13 20:05:24 -0400

    testing chris branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,8 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR and test out
+          chris-branch
         </p>
       </div>
       <p className="read-the-docs">
